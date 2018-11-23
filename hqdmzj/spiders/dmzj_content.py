3b78c7e5cbe76e8dbd55cefe3740ce397fcda41e
shiming-miku <shimingjim@163.com>
2018-11-23 15:02:58 +0800

    循环抓取（获取p标签内部span的文字，但由于p标签内可能不含有span标签的文字，故单独上传一个版本)

--- hqdmzj/spiders/dmzj_content.py
+++ hqdmzj/spiders/dmzj_content.py
@@ -1,26 +1,33 @@
 import scrapy
 
-from hqdmzj.items import HqdmzjItem
+from hqdmzj.items import ContentItem
 
 
 class dmzjContentpider(scrapy.Spider):
     name = "dmzjcontent"
     allowed_domains = ["dmzj.com"]
     start_urls = [
-        "https://news.dmzj.com/article/48308.html"
+        "https://news.dmzj.com/article/48397.html"
     ]
 
     def parse(self, response):
         print response.url
         for sel in response.xpath('//div[@class="news_content_con"]/p'):
-            dmzjitem = HqdmzjItem()
+            contentItem = ContentItem()
+            contentItem['text'] = ''
             try:
                 sel.xpath('.//img/@src').extract()[0]
             except IndexError:
                 try:
-                    dmzjitem['content'] = sel.xpath('.//text()').extract()[0].encode('utf-8')
+                    sel.xpath('.//span').extract()[0]
+                    for self in sel.xpath('.//span'):
+                        contentItem['text'] += self.xpath('.//text()').extract()[0].encode('utf-8')
                 except IndexError:
-                    continue
+                    try:
+                        contentItem['text'] = sel.xpath('.//text()').extract()[0].encode('utf-8')
+                    except IndexError:
+                        continue
+
             else:
-                dmzjitem['content'] = sel.xpath('.//img/@src').extract()[0].encode('utf-8')
-            yield dmzjitem
+                contentItem['text'] = sel.xpath('.//img/@src').extract()[0].encode('utf-8')
+            yield contentItem
